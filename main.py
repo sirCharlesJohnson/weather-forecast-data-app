df6a599ae2ac49a5dda81debf030ae243c78f8f6
sirCharlesJohnson <johcha7@yahoo.com>
2026-04-09 09:19:57 -0400

    Add files via upload

---
+++ main.py
@@ -0,0 +1,18 @@
+import streamlit as st
+import plotly.express as px
+from backend import get_data
+
+st.title("Weather Forecast for the Next Days")
+place = st.text_input("Place: ")
+days = st.slider("Forecast Days", min_value=1, max_value=5,
+                 help="Select the number of forecast days")
+option = st.selectbox("Select data to view",
+                      ("Temperature", "Sky"))
+st.subheader(f"{option} for the next {days} days in {place}")
+
+d, t = get_data(place, days, option)
+
+
+figure = px.line(x=d, y=t, labels={"x": "Date", "y": "Temperature (C)"})
+st.plotly_chart(figure)
+
